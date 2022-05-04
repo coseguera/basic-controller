61b34f0a212eacfde100ae2171c735ec4886fee3
Carlos Oseguera <coseguera@users.noreply.github.com>
2022-05-04 15:41:35 -0700

    setup the main function

--- main.go
+++ main.go
@@ -1,6 +1,18 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"time"
+
+	kubeinformers "k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/klog/v2"
+
+	clientset "github.com/coseguera/basic-controller/pkg/generated/clientset/versioned"
+	informers "github.com/coseguera/basic-controller/pkg/generated/informers/externalversions"
+	"github.com/coseguera/basic-controller/pkg/signals"
+)
 
 var (
 	masterURL  string
@@ -8,6 +20,42 @@ var (
 )
 
 func main() {
+	klog.InitFlags(nil)
+	flag.Parse()
+
+	// set up signals so we handle the first shutdown signal gracefully
+	stopCh := signals.SetupSignalHandler()
+
+	cfg, err := clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
+	if err != nil {
+		klog.Fatalf("Error building kubeconfig: %s", err.Error())
+	}
+
+	kubeClient, err := kubernetes.NewForConfig(cfg)
+	if err != nil {
+		klog.Fatalf("Error building kubernetes clientset: %s", err.Error())
+	}
+
+	basicClient, err := clientset.NewForConfig(cfg)
+	if err != nil {
+		klog.Fatalf("Error building basic clientset: %s", err.Error())
+	}
+
+	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeClient, time.Second*30)
+	basicInformerFactory := informers.NewSharedInformerFactory(basicClient, time.Second*30)
+
+	controller := NewController(
+		kubeClient,
+		basicClient,
+		kubeInformerFactory.Apps().V1().Deployments(),
+		basicInformerFactory.Basiccontroller().V1alpha1().Demos())
+
+	kubeInformerFactory.Start(stopCh)
+	basicInformerFactory.Start(stopCh)
+
+	if err = controller.Run(2, stopCh); err != nil {
+		klog.Fatalf("Error running controller: %s", err.Error())
+	}
 }
 
 func init() {
