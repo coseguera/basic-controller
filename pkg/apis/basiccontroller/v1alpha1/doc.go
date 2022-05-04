7e7467ba68c1da5763863800b714be6ece39d259
Carlos Oseguera <coseguera@users.noreply.github.com>
2022-05-03 17:50:08 -0700

    fix the groupName in doc.go

--- pkg/apis/basiccontroller/v1alpha1/doc.go
+++ pkg/apis/basiccontroller/v1alpha1/doc.go
@@ -15,7 +15,7 @@ limitations under the License.
 */
 
 // +k8s:deepcopy-gen=package
-// +groupName=basiccontroller.k8s.io
+// +groupName=basiccontroller.coseguera.io
 
 // Package v1alpha1 is the v1alpha1 version of the API.
 package v1alpha1 // import "github.com/coseguera/basic-controller/pkg/apis/basiccontroller/v1alpha1"
